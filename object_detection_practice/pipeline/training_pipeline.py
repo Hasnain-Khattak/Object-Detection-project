bb435ad139f8b65776279bad27f5e7e0b8eed3d8
Hasnain-Khattak <msaqibkhan987987@gmail.com>
2024-12-25 18:18:32 +0500

    Updated

--- object_detection_practice/pipeline/training_pipeline.py
+++ object_detection_practice/pipeline/training_pipeline.py
@@ -2,6 +2,7 @@ import sys, os
 from object_detection_practice.exception import SignException
 from object_detection_practice.logger import logging
 from object_detection_practice.components.data_ingestion import DataIngestion
+from object_detection_practice.components.data_validation import DataValidation
 
 from object_detection_practice.entity.config_entity import (DataIngestionConfig,
                                                DataValidationConfig,
@@ -18,6 +19,8 @@ from object_detection_practice.entity.artifacts_entity import (DataIngestionArti
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
+
     
     def start_data_ingestion(self)-> DataIngestionArtifact:
         try: 
@@ -40,6 +43,31 @@ class TrainPipeline:
 
         except Exception as e:
             raise SignException(e, sys)
+        
+        
+    def start_data_validation(
+        self, data_ingestion_artifact: DataIngestionArtifact
+    ) -> DataValidationArtifact:
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config=self.data_validation_config,
+            )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise SignException(e, sys) from e
         
     def run_pipeline(self):
         try:
